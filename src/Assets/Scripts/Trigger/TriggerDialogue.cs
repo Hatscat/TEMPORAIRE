c4b102e535068b2020827afcbd91ea9a065b5f4c
Cutter <s.levron.pro@gmail.com>
2015-06-13 18:31:18 +0200

    Add dialogueSystem and first step dialogue

---
+++ src/Assets/Scripts/Trigger/TriggerDialogue.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+namespace PixelCrushers.DialogueSystem.Examples {
+	
+	public class TriggerDialogue : MonoBehaviour {
+		
+		public string message;
+		
+		public void OnTriggerEnter(Collider other) {
+			//DialogueManager.ShowAlert("Got " + name);
+			Sequencer.Message(message);
+			Destroy(gameObject);
+		}
+		
+	}
+	
+}
