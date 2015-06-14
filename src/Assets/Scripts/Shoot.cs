873b992baaaa249bf1e55d4d9a5e1c94c35364f0
Cutter <s.levron.pro@gmail.com>
2015-06-14 18:31:49 +0200

    Commit

---
+++ src/Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject Bullet;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void shoot()
+    {
+        GameObject go =
+                Instantiate(Bullet,
+                            transform.position,
+                            Bullet.transform.rotation)
+                    as GameObject;
+        go.GetComponent<NewBehaviourScript>().direction = transform.forward;
+    }
+}
