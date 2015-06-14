ca1ab215fd977bd9427e35570635497a51d121ff
f.wax <f.wax@student.isartdigital.com>
2015-06-14 07:56:57 +0200

    Debut AI

---
+++ src/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public float speed;
+    public Vector3 direction;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
