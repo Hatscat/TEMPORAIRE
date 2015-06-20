b696e8e3bef97840c359e93f1f0e230e452e1828
f.wax <f.wax@student.isartdigital.com>
2015-06-20 18:00:45 +0200

    FIX AI mob turret + FX

---
+++ src/Assets/_Scripts/Components/mobs/AutoDestroy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroy : MonoBehaviour {
+
+    public float timer;
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (timer != 0)
+            timer = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+            Destroy(gameObject);
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("HIT!");
+        Destroy(gameObject);
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Enter called");
+    }
+}
