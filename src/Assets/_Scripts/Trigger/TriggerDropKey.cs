181131233e4ee31950070175ffd25d3d0983a139
Cutter <s.levron.pro@gmail.com>
2015-06-21 21:14:53 +0200

    Interraction avec pnj

---
+++ src/Assets/_Scripts/Trigger/TriggerDropKey.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using PixelCrushers.DialogueSystem;
+using System;
+
+public class TriggerDropKey : MonoBehaviour {
+	
+	public string name; //Inspector
+	
+	// Use this for initialization
+	void Start () 
+	{
+		//GameObject newObject = Instantiate(go_objectMDL, go_SpawnObject.transform.position,transform.rotation) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void OnConversationEnd(string name) {
+		switch (name){
+		case "Armor":
+			PlayerManager.manager.key_armory = true;
+			break;
+		case "ImplantA":
+			PlayerManager.manager.key_implant_A = true;
+			break;
+		case "ImplantB":
+			PlayerManager.manager.key_implant_B = true;
+			break;
+		case "Area2":
+			PlayerManager.manager.key_area_2 = true;
+			break;
+		case "Cokpit":
+			PlayerManager.manager.key_cokpit = true;
+			break;
+		case "Black":
+			PlayerManager.manager.key_black_material = true;
+			break;
+		case "Ammo":
+			PlayerManager.manager.key_room_ammo = true;
+			break;
+		}
+	}
+}
