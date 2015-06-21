d98c8592554c0567f2c7c6c270fe363314380d71
Cutter <s.levron.pro@gmail.com>
2015-06-21 18:28:40 +0200

    Fix trigger machine teleportation

---
+++ src/Assets/_Scripts/Trigger/TriggerDropCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using PixelCrushers.DialogueSystem;
+
+public class TriggerDropCollider : MonoBehaviour {
+	
+	public GameObject go_objectMDL; //Inspector
+	
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void OnConversationEnd(Transform go_SpawnObject) {
+		//GameObject newObject = Instantiate(go_objectMDL, go_SpawnObject.position,transform.rotation) as GameObject;
+		go_objectMDL.GetComponent<MeshCollider> ().isTrigger = true;
+	}
+}
