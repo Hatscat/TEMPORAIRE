732d53b0fe4b0d920e0bb8fba27b746933369a7e
Lucien Boudy <luboudy@gmail.com>
2015-06-22 11:02:37 +0200

    post process ASCII effect

---
+++ src/Assets/_Scripts/PostProcess/AsciiEffect.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+public class AsciiEffect : ImageEffectBase
+{
+    // Called by camera to apply image effect
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
